38a262cf8d776196d31be74fe914a7b6cf17ecc6
unknown <kozachokdima2006@gmail.com>
2025-02-05 13:37:15 +0200

    Start

---
+++ Vocaquill/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Vocaquill
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
