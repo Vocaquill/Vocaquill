f3545348c45b6fe37380365d705130baec03025a
vladhardetskyy@gmail.com <13238274Vv>
2025-02-08 19:15:11 +0200

    1. Change ConstantClass to DataAccessConstants 2. Add Context, Interfaces and Repositories to DAL 3. Add DTO Models, Interfaces and Services to BLL

---
+++ BLL/Interfaces/IQueryService.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IQueryService
+    {
+        Task<ICollection<QueryDTO>> GetQueriesAsync();
+        Task<QueryDTO> GetQueryByIdAsync(int id);
+        Task<ICollection<QueryDTO>> GetQueriesByUserIdAsync(int userId);
+        Task AddQueryAsync(QueryDTO query);
+        Task UpdateQueryNameAsync(int id, string name);
+        Task DeleteQueryAsync(int id);
+    }
+}
---
+++ DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(int id, User user);
+        Task DeleteUserAsync(int id);
+    }
+}
