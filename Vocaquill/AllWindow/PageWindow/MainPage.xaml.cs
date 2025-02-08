1536f426b611ca5d118bc82145390b44acd974f2
Костянтин Підодвірний <kostya190807@gmail.com>
2025-02-08 01:24:10 +0100

    Creating a basic UI

---
+++ Vocaquill/AllWindow/PageWindow/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vocaquill.AllWindow.PageWindow
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void StartStopRecordBT_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LogoutBT_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = (MainWindow)Application.Current.MainWindow;
+            mainWindow.modalFrame.Navigate(new LoginWindowPage());
+        }
+    }
+}
