1e6e01f76764e1fbc186a5b43175b6934bdd4a33
Petro <petrotymchuck@gmail.com>
2025-02-10 18:08:30 +0200

    Create MVVM base structure

--- Vocaquill/MainWindow.xaml.cs
+++ Vocaquill/MainWindow.xaml.cs
@@ -1,26 +1,16 @@
 ﻿using System.ComponentModel;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Vocaquill.AllWindow.PageWindow;
-using Vocaquill.Commands;
 
 namespace Vocaquill
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
             modalFrame.Content = new LoginWindowPage();
         }
-        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
